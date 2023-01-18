783b30231dad43539ace9e811d7f63c947fe82b8
Richard Ortiz <raortiz20@utpl.edu.ec>
2023-01-18 15:47:37 -0500

    Clase062

--- EjemplosDos/src/paquete02/DatoTrabajo.java
+++ EjemplosDos/src/paquete02/DatoTrabajo.java
@@ -5,12 +5,31 @@
  */
 package paquete02;
 
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
  */
 public class DatoTrabajo {
+
+    public static String empresa(){
+        Scanner entrada = new Scanner(System.in);
+        String empresa ;
+        System.out.println("Ingrese el nombre de la empresa");
+        empresa = entrada.nextLine();
+        return empresa;
+
+    }
+
+    public static String direccion(){
+        Scanner entrada = new Scanner(System.in);
+        String direccion;
+        System.out.println("Ingrese la direccion de la empresa");
+        direccion = entrada.nextLine();
+        return direccion;
+    }
     // método que permita preguntar al usuario el nombre de la empresa
-    
+
     // método que permita preguntar al usuario la dirección de la empresa
 }
