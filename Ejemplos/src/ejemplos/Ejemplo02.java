7efa7092fd67cf3150a97445c7efb3d4ff1b16d2
Richard Ortiz <raortiz20@utpl.edu.ec>
2023-01-18 14:58:37 -0500

    Clase06

--- Ejemplos/src/ejemplos/Ejemplo02.java
+++ Ejemplos/src/ejemplos/Ejemplo02.java
@@ -21,13 +21,19 @@ public class Ejemplo02 {
         int limite;
         System.out.println("Ingrese el limite de los numeros ramdom");
         limite = entrada.nextInt();
-        int valorA = obtenerNumero(limite); // 3
+        if (limite > 0){
+             int valorA = obtenerNumero(limite); // 3
         int valorB = obtenerNumero(limite); // 2
         int suma = obtnerSuma(valorA, valorB);
         System.out.printf("La suma de %d + %d es igual a: %d\n", 
                 valorA,
                 valorB,
                 suma);
+            
+        }else{
+            System.out.println("Error: limite incorrecto");
+        }
+       
     }
     
     public static int obtenerNumero(int limite) {
