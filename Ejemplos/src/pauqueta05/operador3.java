b41e010fccf0fdca9a866e05bf68954eb58dfa7c
Richard Ortiz <raortiz20@utpl.edu.ec>
2023-01-18 15:25:08 -0500

    Clase06

---
+++ Ejemplos/src/pauqueta05/operador3.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pauqueta05;
+
+/**
+ *
+ * @author SALA I
+ */
+public class operador3 {
+
+    public static int multiplicacion(int a, int b) {
+        return a * b;
+
+    }
+}
